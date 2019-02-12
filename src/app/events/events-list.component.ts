29f4c631114136b3e8eede9471b8e16f26b49c5c
linazoo <linazooper@gmail.com>
2019-02-11 21:31:40 -0600

    Using External Templates and communicating with child components

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -3,26 +3,16 @@ import { Component } from '@angular/core'
 @Component ({
   selector: 'events-list',
   template: `
-  <div>
-    <h1>Upcoming Angular Events</h1>
-    <hr>
-    <hr/>
-    <div class="well hoverwell thumbail">
-      <h2>{{event.name}}</h2>
-      <div>Date: {{event.date}}</div>
-      <div>Time: {{event.time}}</div>
-      <div>Price: \${{event.price}}</div>
-      <div>
-        <span>Location: {{event.location.address}}</span>
-        <span>&nbsp;</span>
-        <span>{{event.location.city}}, {{event.location.country}}</span>
-      </div>
-    </div>
-  </div> 
-  `
+    <div>
+      <h1>Upcoming Angular Events</h1>
+      <hr>
+      <hr/>
+      <event-thumbnail [event]="event1"></event-thumbnail>
+    </div> 
+    `
 })
 export class EventsListComponent {
-  event = {
+  event1 = {
     id: 1, 
     name: 'Angular Connect',
     date: '9/26/2036',
